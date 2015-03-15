934cd5691d9a33c080fee4e473a401bc3fb96578
Ludwig Gustavsson <thesparvgames@gmail.com>
2015-03-15 15:21:22 +0100

    Test map + movement update

--- Racing/Assets/Scripts/CarMovement.cs
+++ Racing/Assets/Scripts/CarMovement.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class CarMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    float speed = 60.0f;
+    float turnSpeed = 5.0f;
+    float hoverForce = 65.0f;
+    float hoverHeight = 3.5f;
+    float powerInput;
+    float turnInput;
+    Rigidbody myRigidbody;
+
+	void Start () 
+    {
+        myRigidbody = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        powerInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
 	}
+
+    void FixedUpdate()
+    {
+        //RaycastHit hit;
+        //Ray ray = new Ray(transform.position, -transform.up);
+
+        //if (Physics.Raycast(ray, out hit, hoverHeight))
+        //{
+        //    float proportionalHeight = (hoverHeight - hit.distance) / hoverHeight;
+        //    Vector3 appliedForce = Vector3.up * proportionalHeight * hoverForce;
+        //    myRigidbody.AddForce(appliedForce, ForceMode.Acceleration);
+        //}
+
+        myRigidbody.AddRelativeForce(0.0f, 0.0f, powerInput * speed);
+        transform.Rotate(Vector3.up, turnInput * turnSpeed);
+    }
 }
---
+++ Racing/Assets/Scripts/DisableRenderer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableRenderer : MonoBehaviour {
+
+    RaycastHit hit;
+    GameObject[] walls;
+
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+        walls = GameObject.FindGameObjectsWithTag("Wall");
+
+        Debug.DrawRay(transform.position, transform.forward * 6);
+        Ray ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out hit, 6))
+        {
+            if (hit.collider.tag == "Wall")
+                hit.collider.GetComponentInChildren<Renderer>().enabled = false;
+        }
+        else
+        {
+            for (int i = 0; i < walls.Length; i++)
+            {
+                walls[i].GetComponent<Renderer>().enabled = true;
+            }
+        }
+	}
+}
